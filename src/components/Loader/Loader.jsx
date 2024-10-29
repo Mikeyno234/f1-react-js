eef8e8364e92a95344a055dacae48f5320bf3ed1
Mikeyno234 <mulyanaputrahanif@gmail.com>
2024-10-29 08:23:06 +0700

    updte loader guys

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { Vortex } from "react-loader-spinner";
+import { InfinitySpin } from "react-loader-spinner";
 import "./Loader.css";
 
 function Loader() {
   return (
     <div className="Loader">
-      <Vortex
-        visible={true}
-        height="100"
-        width="100"
-        ariaLabel="infinity-spin-loading"
-        wrapperStyle={{}}
-        wrapperClass="infinity-wrapper"
-        colors={["#ffff", "#c42021", "#dd2c2c", "#e56161", "#005ba9", "#fd48c7"]}
+      <InfinitySpin
+          visible={true}
+          width="200"
+          color="#ff0000"
+          ariaLabel="infinity-spin-loading"
       />
     </div>
   );
